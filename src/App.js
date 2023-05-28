e57b10b5cb4f552b7bcbb097595ae8e29e584c5b
thegolumehra <golucodes@gmail.com>
2023-05-28 21:43:09 +0530

    can take turns now

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
-export default function Square() {
-  return <button className="square">X</button>;
-}
+import React from 'react';
+import Game from './components/Game.js';
+
+const App = () => {
+  return (
+    <>
+      <h1>Tic Tac Toe</h1>
+      <Game />
+    </>
+  );
+};
+
+export default App;
